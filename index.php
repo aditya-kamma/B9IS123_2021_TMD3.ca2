99fb5d37bc08f313418041a999097fe3d861fdfc
Aditya Kamma <86240491+aditya-kamma@users.noreply.github.com>
2021-07-19 21:33:35 +0100

    Add files via upload
    index

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php 
+?> 
+	<div id="banner">
+    	<div class="left">
+          <div class="wrapper">
+            <ul>
+               <li><a href="#"><img src="./images/banner1.jpg" alt="" /></a></li>
+            </ul>        
+          </div>
+          
+        </div>
+    </div>
+
+  <div id="content_sec">
+     <div class="col1">
+		<h4 class="heading colr">Employee Management System</h4>
+      <p>An employee management system is a platform where all work-related as well as important personal details of an employee is stored and managed in a secure way. By using this system, we can manage admin activities in an easier and quicker way.
+
+Employees are the pillar of any organization and an ideal employee management tool makes a big difference to an organization.</p>
+<br>
+<p>Tracing and recording of the employee data had never been easier. Creating a proper employee database is an important to-do task for perfect payroll. Our online employee management software is an essential tool. and creates the required database for every month. Also, automation helps in cutting down errors. The online employee management system lets your HR work from any remote location as well.</p>
+ </div>
+	<div class="col2">
+		<div><img src="images/save_2.jpg" style="width: 250px"></div>
+	</div>
+    <div class="clear"></div>
+  </div>
+  <div class="clear"></div>
+</div>
+<?php  ?> 
