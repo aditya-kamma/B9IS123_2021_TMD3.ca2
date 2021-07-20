0f05c7377409cfbfa908d03552e0bfef5d49d447
kollurisukesh <kolluri.sukesh@gmail.com>
2021-07-20 22:54:27 +0100

    contact 2/2

---
+++ contact-confirmation.php
@@ -0,0 +1,36 @@
+<?php  ?> 
+	<div class="crumb">
+    	<p>You are Here:</p>
+        <ul>
+        	<li class="first"><a href="#">Home</a></li>
+            <li><a href="#">About Us</a></li>
+        </ul>
+    </div>
+    <div class="clear"></div>
+	<div id="content_sec">
+		<div class="col1">
+			<div class="contact">
+				<h4 class="heading colr">Contact Us</h4>
+				<div>Thank you for contacting us. We will get back to you soon !!!!</div>
+			</div>
+		</div>
+		<div class="col2">
+			<div class="contactfinder">
+				<h4 class="heading colr">Where to find us.</h4>
+				<a href="#" class="mapcont"><img src="./images/contact.jpg" alt="" style="width:250px;"/></a>
+				<h4>Get in touch</h4>
+				<p>
+				  			You’ll find our office <br />
+							Opposite to AIB bank<br />
+					<br />
+					10 Molesworth Street<br />
+					Dublin 2<br />
+					D02 TC9V<br />
+					<br /><br />
+					+353 (0) 12345678 <br />
+					<a href="">info@system.com</a><br />
+				</p>
+			</div>
+		</div>
+	</div>
+<?php  ?> 
