d981b61673836c2946c7006d8b549f131262fa04
Aditya Kamma <86240491+aditya-kamma@users.noreply.github.com>
2021-07-22 02:29:14 +0100

    Update employee_management_system.sql
    Updated Database

--- employee_management_system.sql
+++ employee_management_system.sql
@@ -1,11 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.6.5.2
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Generation Time: Jul 06, 2021 at 10:00 AM
--- Server version: 5.5.53-0ubuntu0.14.04.1
--- PHP Version: 5.6.31-4+ubuntu14.04.1+deb.sury.org+4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -15,6 +7,8 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
+
+
 
 --
 -- Database: `employee_management_system`
@@ -60,6 +54,9 @@ INSERT INTO `country` (`country_id`, `country_name`) VALUES
 
 -- --------------------------------------------------------
 
+--
+-- Table structure for table `gender`
+--
 
 
 
@@ -98,9 +95,9 @@ CREATE TABLE `employee` (
 --
 
 INSERT INTO `employee` (`employee_id`, `employee_name`, `employee_add1`, `employee_add2`, `employee_city`, `employee_country`, `employee_email`, `employee_mobile`, `employee_gender`, `employee_dob`, `employee_image`) VALUES
-(2, 'Aditya Kamma', 'Bridgefield', 'Dublin 09', '2', '1', 'aaditkamma3@gmail.com', '0899764905', '1', '3 November,1996', 'p2.jpg'),
-(3, 'Sukesh Kolluri', 'Near spire', 'Dublin 01', '1', '1', 'kolluri.sukesh@gmail.com', '0897145678', '1', '5 February, 1997', 'p3.jpg'),
-(4, 'Shiju Chandra', 'Lucan', 'Dublin 13', '1', '1', 'shiju.chandra@gmail.com', '0897867564', '1', '10 January, 1986', 'p4.jpg');
+(2, 'Aditya Kamma', 'Bridgefield', 'Dublin 09', '2', '1', 'aaditkamma3@gmail.com', '0899764905', '1', '03-11-1996', 'p2.jpg'),
+(3, 'Sukesh Kolluri', 'Near spire', 'Dublin 01', '1', '1', 'kolluri.sukesh@gmail.com', '0897145678', '1', '05-01-1997', 'p3.jpg'),
+(4, 'Shiju Chandra', 'Lucan', 'Dublin 13', '1', '1', 'shiju.chandra@gmail.com', '0897867564', '1', '13-11-1987', 'p4.jpg');
 -- --------------------------------------------------------
 
 --
@@ -120,8 +117,8 @@ CREATE TABLE `login` (
 --
 
 INSERT INTO `login` (`login_id`, `login_user`, `login_password`, `login_level`, `login_date`) VALUES
-(1, 'admin', 'test', '', NULL);
-
+(1, 'admin', 'test', '', NULL),
+(2, 'Sukesh', 'test', '', NULL);
 -- --------------------------------------------------------
 
 --
@@ -150,26 +147,6 @@ INSERT INTO `month` (`month_id`, `month_name`) VALUES
 (10, 'October'),
 (11, 'November'),
 (12, 'December');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `role`
---
-
-CREATE TABLE `role` (
-  `role_id` int(11) NOT NULL,
-  `role_name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `role`
---
-
-INSERT INTO `role` (`role_id`, `role_name`) VALUES
-(1, 'Super Admin'),
-(2, 'Head Of Department'),
-(3, 'Faculty');
 
 -- --------------------------------------------------------
 
@@ -230,8 +207,8 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`user_id`, `user_package_id`, `user_shift_id`, `user_level_id`, `user_username`, `user_password`, `user_name`, `user_add1`, `user_add2`, `user_city`, `user_country`, `user_email`, `user_mobile`, `user_gender`, `user_dob`, `user_image`) VALUES
-(8, '2', '3', '1', 'admin', 'test', 'Aditya kamma', 'Bridgefield', 'Dublin 09', '1', '1', 'aaditkamma3@gmail.com', '0899764905', '', '03 November,1996', 'IMG_5748.jpg');
-
+(1, '2', '3', '1', 'admin', 'test', 'Aditya kamma', 'Bridgefield', 'Dublin 09', '1', '1', 'aaditkamma3@gmail.com', '0899764905', '', '03 November,1996', 'IMG_5748.jpg'),
+(2, '2', '3', '1', 'Sukesh', 'test', 'Sukesh', 'Bridgefield', 'Dublin 09', '1', '1', 'sukesh@gmail.com', '089786756', '', '03 November,1996', 'IMG_5748.jpg');
 --
 
 
@@ -266,22 +243,16 @@ ALTER TABLE `month`
   ADD PRIMARY KEY (`month_id`);
 
 --
--- Indexes for table `role`
---
-ALTER TABLE `role`
-  ADD PRIMARY KEY (`role_id`);
-
---
 -- Indexes for table `salary`
 --
 ALTER TABLE `salary`
   ADD PRIMARY KEY (`salary_id`);
 
 --
+-- Indexes for table `gender`
+--
 ALTER TABLE `gender`
 ADD PRIMARY KEY (`gender_id`);
-
---
 
 
 --
@@ -317,10 +288,6 @@ ALTER TABLE `login`
 ALTER TABLE `month`
   MODIFY `month_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 --
--- AUTO_INCREMENT for table `role`
---
-ALTER TABLE `role`
-  MODIFY `role_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `salary`
 --
